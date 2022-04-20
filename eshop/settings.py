c92bac996caa53a20d75f1757f5b106f97a99d0c
sandeep <sandeepsoni2291@gmail.com>
2022-04-20 19:33:17 +0530

    TCP/IP Localhost to Remote

--- eshop/settings.py
+++ eshop/settings.py
@@ -84,7 +84,8 @@ WSGI_APPLICATION = 'eshop.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 import dj_database_url
-DATABASES = { 'default': dj_database_url.config() }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # DATABASES = {
 #     'default': {
