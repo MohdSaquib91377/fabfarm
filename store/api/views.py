e0eb0a327d331d249888791607dcfe6355f60ab4
sandeep <sandeepsoni2291@gmail.com>
2022-04-27 15:03:29 +0530

    Update store/api/views.py

--- store/api/views.py
+++ store/api/views.py
@@ -1,15 +1,19 @@
 from rest_framework.views import APIView
 from store.models import *
 from rest_framework.response import Response
+from django.utils.decorators import method_decorator
 from .serializers import CategorySerializer,ProductsSerializer,ProductsDetailsSerializer
+from django.views.decorators.csrf import csrf_exempt
 
-class CategoryListView(APIView):
+@method_decorator(csrf_exempt, name='dispatch')
+class CategoryListView(APIView):    
     def get(self,request,*args, **kwargs):
         queryset = Category.objects.all()
 
         serializer = CategorySerializer(queryset,many=True)
         return Response(serializer.data) 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class ProductsListView(APIView):
     def get(self,request,*args, **kwargs):
         catAll = Category.objects.all()
@@ -23,6 +27,8 @@ class ProductsListView(APIView):
             data.append({"category":category.name,"data":serializer.data})
         return Response(data) 
 import json
+
+@method_decorator(csrf_exempt, name='dispatch')
 class ProductDetailsView(APIView):
     def get(self,request,product_id,*args, **kwargs):
         queryset = Product.objects.filter(pk=product_id)
