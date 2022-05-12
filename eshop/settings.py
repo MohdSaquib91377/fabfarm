22501e7a11e0045bebcd4b857796e52470231525
Mrsaqib <saqib@dataviv.in>
2022-05-12 05:49:28 +0000

    server changes

--- eshop/settings.py
+++ eshop/settings.py
@@ -126,10 +126,16 @@ OTP = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'agriculture_db',
+        'USER': 'admin',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
