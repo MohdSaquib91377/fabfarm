78391eca2f853d9e65230bb1352e757c787b535b
Mrsaqib <saqib@dataviv.in>
2022-05-12 06:53:38 +0000

    store urls has been modified

--- store/api/urls.py
+++ store/api/urls.py
@@ -3,6 +3,6 @@ from store.api import views
 urlpatterns = [
     path('category/', views.CategoryListView.as_view(),name='category-view'),
     path('category-product/', views.CategoryProductView.as_view(),name='category-product'),
-    path('category/<category_id>/',views.CategoryDetailsView.as_view(),name='category-details'),
-    path('product/<product_id>/', views.ProductDetailsView.as_view(),name='product-details'),    
+    path('category-details/<category_id>/',views.CategoryDetailsView.as_view(),name='category-details'),
+    path('product-details/<product_id>/', views.ProductDetailsView.as_view(),name='product-details'),    
 ]
