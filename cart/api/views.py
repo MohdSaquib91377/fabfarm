3ced2c619274a901a52686c7b058cc155efe2244
unknown <saqib@dataviv.in>
2022-05-13 19:09:32 +0530

    cart api implemented

--- cart/api/views.py
+++ cart/api/views.py
@@ -1,19 +1,61 @@
 from rest_framework.views import APIView
-from store.models import *
+from cart.models import *
 from rest_framework.response import Response
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.permissions import IsAuthenticated
+from.serializers import CartSerializer,CreateCartSerializer
+from rest_framework import status
+from store.helpers import get_product_object
 
 class AddToCartApiView(APIView):
-    
+    permission_classes = [IsAuthenticated]
     def get(slef,request,*args,**kwargs):
-        pass
+        try:
+            queryset = Cart.objects.filter(user = request.user)
+            serializer = CartSerializer(queryset,many=True,context = {'user':request.user})
+            cart_total,cart_item = Cart.get_cart_total_item_or_cost(request.user)
+            new_serializer_data = list(serializer.data)
+            new_serializer_data.append({"cart_total":cart_total,"cart_item":cart_item})
+            return Response(new_serializer_data)
+        except Exception as e:
+            return Response({"status":"400","message":f"{e}"},status = status.HTTP_400_BAD_REQUEST)
 
     def post(self,request,*args,**kwargs):
-        pass
+        try:
+            serializer = CreateCartSerializer(data = request.data)
+            product = get_product_object(request.data.get('product_id'))
+            if serializer.is_valid():
+                if Cart.objects.filter(user = request.user, product = product).exists():
+                    Cart.objects.filter(user = request.user, product = product).update(quantity = serializer.data['quantity'])
+                    return Response({"sttaus":"200","message":"Product updated into Cart"},status = status.HTTP_400_BAD_REQUEST)
+                serializer.save(user = request.user)
+                return Response({"sttaus":"200","message":"Product Added into Cart"},status = status.HTTP_201_CREATED)
+            return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"status":"400","message":f"{e}"},status= status.HTTP_400_BAD_REQUEST)
 
     def put(self,request, *args, **kwargs):
-        pass
-    
+        try:
+            product = get_product_object(request.data.get('product_id'))
+            user_cart = Cart.objects.filter(user = request.user, product = product).first()
+            if user_cart:
+                serializer = CreateCartSerializer(user_cart,data=request.data, partial=True)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response({"status":"200","message":"Product updated into cart successfully"},status = status.HTTP_200_OK)
+                return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+            return Response({"status":"400","message":"You dont have permission to edit this product"},status = status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"status":"400","message":f"{e}"},status = status.HTTP_400_BAD_REQUEST)
+
     def delete(self,request, *args, **kwargs):
-        pass
+        try:
+            product = get_product_object(request.data.get('product_id'))
+            user_cart = Cart.objects.filter(user = request.user, product = product).first()
+            if user_cart:
+                user_cart.delete()
+                return Response({"status":"200","message":"Product deleted successfully"},status = status.HTTP_204_NO_CONTENT)
+            return Response({"status":"400","message":"You dont have permission to edit this product"},status = status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({"status":"400","message":f"{e}"},status = status.HTTP_400_BAD_REQUEST)
--- cart/models.py
+++ cart/models.py
@@ -1,26 +1,24 @@
 from django.db import models
-from django.db.models.signals import post_save,pre_save
-from django.dispatch import receiver
 # Create your models here.
 from store.models import Product
 from account.models import CustomUser,TimeStampModel
 
 class Cart(TimeStampModel):
-    ordered = models.BooleanField(default=False)
     user = models.ForeignKey('account.CustomUser',on_delete=models.CASCADE,null=True)
-    total_price = models.FloatField(default=0)
-    total_item = models.IntegerField(default=0)
+    product = models.ForeignKey('store.Product',on_delete=models.CASCADE,null=True)
+    quantity = models.IntegerField(default=0)
     
-class CartItem(TimeStampModel):
-    cart = models.ForeignKey('Cart',related_name="cart_items",on_delete=models.CASCADE)
-    product = models.ForeignKey('store.Product',on_delete=models.CASCADE)
-    price = models.IntegerField(default = 0)
-    quantity = models.IntegerField()
+    class Meta:
+        ordering = ['-id']
 
+    @staticmethod
+    def get_cart_total_item_or_cost(user):
+        carts = Cart.objects.filter(user=user)
+        cart_total = 0
+        cart_item = 0
+        for cart in carts:
+            cart_total = cart_total + int(cart.quantity) * int(cart.product.price)
+            cart_item = cart_item + int(cart.quantity) 
+        return cart_total,cart_item
 
-@receiver(post_save, sender=CartItem)
-def Calc_total_item_and_total_price(sender,**kwargs):
-    cart_item_instance = kwargs['instance']
-    product = Product.objects.filter(id = cart_item_instance.product_id).first()
-    product_total_price = cart_item_instance.quantity * int(product.price)
-    print("i got called")
+    
