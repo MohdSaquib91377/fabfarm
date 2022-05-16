c60d094fc9c7e7f86e68e9936cebcf765e64fe98
unknown <saqib@dataviv.in>
2022-05-16 17:02:25 +0530

    maxQuantity and initial quantity set

--- store/api/serializers.py
+++ store/api/serializers.py
@@ -20,15 +20,20 @@ class ProductsSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField("get_images")
     category = serializers.CharField(source="category.name", read_only=True)
     brand = serializers.CharField(source="brand.name", read_only=True)
+    maxQuantity = serializers.IntegerField(source = "quantity")
+    quantity = serializers.SerializerMethodField("set_qauntity_by_1")
+
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = ["id","name","slug","sku","price","old_price","is_active","is_bestseller","maxQuantity","quantity","meta_keywords","meta_description","category","brand","image"]
         
     def get_images(self, obj):
         images = obj.images.all()
         serializer = ImageSerializer(images,many=True)
         return serializer.data
 
+    def set_qauntity_by_1(self,obj):
+        return 1
 
 class CategoryProductSerializer(serializers.ModelSerializer):
     products = ProductsSerializer(many=True, read_only=True)
