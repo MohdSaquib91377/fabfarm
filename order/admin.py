65035abc8cea2bdaa714f07cfa273460b0cd6c6a
unknown <saqib@dataviv.in>
2022-05-19 17:38:27 +0530

    order module done

---
+++ order/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from order.models import *
+# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["user","full_name","city","state","country",
+                    "pincode","locality","landmark","address",
+                    "alternate_number","total_price","payment_mode",
+                    "payment_id","message","tracking_no"
+    ]
+admin.site.register(Order,OrderAdmin)
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ["order","product","price","quantity","status"]
+
+admin.site.register(OrderItem,OrderItemAdmin)
---
+++ order/api/serializers.py
@@ -0,0 +1,30 @@
+from attr import field
+from rest_framework.response import Response
+from rest_framework import serializers
+from order.models import Order,OrderItem
+from account.api.serializers import *
+from store.api.serializers import *
+from store.models import *
+
+class OrderSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(read_only = True)
+    class Meta:
+        model = Order
+        fields = ["full_name","city","state","country","pincode","locality","landmark","address","alternate_number","payment_mode","message","user"]
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductsSerializer()
+    order = serializers.PrimaryKeyRelatedField(read_only = True)
+
+    class Meta:
+        model = OrderItem
+        fields = "__all__"  
+
+class OrderItemDetailsSerializer(serializers.ModelSerializer):
+    product = ProductsSerializer()
+    order = OrderSerializer()
+
+    class Meta:
+        model = OrderItem
+        fields = "__all__"  
