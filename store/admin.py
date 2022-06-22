cb1ed90102bdf26f0df919f0f4e4d21d1b178e56
unknown <saqib@dataviv.in>
2022-06-22 19:25:08 +0530

    sub cateory implemented

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category, Brand, Product, Image, RecentView
+from .models import Category, Brand, Product, Image,SubCategory
 
 class CategoryAdmin(admin.ModelAdmin):    
-    list_display = ['id', 'name', 'slug','description','is_active','meta_keywords','meta_description']    
+    list_display = ['id', 'name', 'slug','is_active','meta_keywords','meta_description']    
 admin.site.register(Category, CategoryAdmin)
 
+class SubCategoryAdmin(admin.ModelAdmin):    
+    list_display = ['id', 'name','category', 'slug','is_active','meta_keywords','meta_description']    
+admin.site.register(SubCategory, CategoryAdmin)
 class BrandAdmin(admin.ModelAdmin):    
     list_display = ['id', 'name', 'slug','meta_keywords','meta_description']
 admin.site.register(Brand, BrandAdmin)
@@ -22,7 +25,3 @@ class ProductAdmin(admin.ModelAdmin):
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Image)
 
-class RecentViewAdmin(admin.ModelAdmin):
-    list_display = ["id","user","product","views_counter"]
-
-admin.site.register(RecentView, RecentViewAdmin)
--- store/api/serializers.py
+++ store/api/serializers.py
@@ -11,23 +11,16 @@ class ImageSerializer(serializers.ModelSerializer):
         model = Image 
         fields = "__all__"
 
-class CategorySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Category
-        fields = "__all__"
 
 
 class ProductsSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField("get_images")
-    category = serializers.CharField(source="category.name", read_only=True)
     brand = serializers.CharField(source="brand.name", read_only=True)
     maxQuantity = serializers.IntegerField(source = "quantity")
     quantity = serializers.SerializerMethodField("set_qauntity_by_1")
-    recently_viwed = serializers.SerializerMethodField("get_recently_viewed_product")
     class Meta:
         model = Product
-        fields = ["id","name","slug","sku","price","old_price","is_active","is_bestseller","maxQuantity","quantity","description","meta_keywords","meta_description","category","brand","image","recently_viwed"]
+        fields = ["id","name","slug","sku","price","old_price","is_active","is_bestseller","maxQuantity","quantity","description","meta_keywords","meta_description","brand","image"]
         
     def get_images(self, obj):
         images = obj.images.all()
@@ -37,27 +30,22 @@ class ProductsSerializer(serializers.ModelSerializer):
     def set_qauntity_by_1(self,obj):
         return 1
 
-    def get_recently_viewed_product(self,obj):
-        data = dict()
-        list = []
-        queryset = RecentView.objects.filter(user = self.context['user'])
-        for item in queryset:
-            data["id"] = item.product.id
-            data["name"] = item.product.name
-            data["sku"] = item.product.sku
-            data["price"] = item.product.price
-            data["old_price"] = item.product.old_price
-            data["quantity"] = item.product.quantity
-            data["images"] = item.product.images.filter().first().image.url
-            list.append(data)
-            data = {}
-        return list
         
-class CategoryProductSerializer(serializers.ModelSerializer):
+class SubCategoryProductSerializer(serializers.ModelSerializer):
     products = ProductsSerializer(many=True, read_only=True)
-
     class Meta:
-        model = Category
+        model = SubCategory
         fields = ['id', 'name','products']
 
+class SubCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubCategory
+        fields = ['id', 'name']
 
+class CategorySerializer(serializers.ModelSerializer):
+    sub_categories = SubCategorySerializer(many=True, read_only=True)
+    class Meta:
+        model = Category
+        fields = ["id","name","sub_categories"]
+
+ 
