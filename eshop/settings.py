b4ffced79bd00f16f2e103cdfc0b26314c57a5ad
AlexDataViv <saqib@dataviv.in>
2022-07-15 16:24:34 +0530

    celery integrated

--- eshop/settings.py
+++ eshop/settings.py
@@ -244,3 +244,12 @@ ELASTICSEARCH_DSL = {
         'hosts': 'localhost:9200'
     },
 }
+
+# redis configuration
+# CELERY STUFF
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Kolkata'
--- services/email.py
+++ services/email.py
@@ -1,5 +1,7 @@
 from django.core import mail
 from django.conf import settings
+from celery import shared_task
 
+@shared_task(name="send_mail")
 def send_mail(subject:str, body:str,to_email:str) ->None :
     mail.send_mail(subject, body,settings.EMAIL_HOST_USER,to_email,fail_silently=False)
