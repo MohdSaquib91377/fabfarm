3deb4adef453eb2cd07128d1aaf7e135269602a8
AlexDataViv <saqib@dataviv.in>
2022-07-25 17:29:50 +0530

    coupon admin issues fixed

--- coupon/models.py
+++ coupon/models.py
@@ -1,4 +1,6 @@
+from email.policy import default
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
@@ -8,14 +10,19 @@ class Coupon(TimeStampModel):
     couponCode = models.CharField(max_length = 64)
     percentageFlate = models.BooleanField(default=True)
     discountValue = models.CharField(max_length =64)
-    maximumDiscountValue = models.IntegerField()
-    couponApplyCount = models.IntegerField(default = 0)
-    maxApplyCount = models.IntegerField()
+    maximumDiscountValue = models.PositiveIntegerField()
+    couponApplyCount = models.PositiveIntegerField(default = 1)
+    maxApplyCount = models.PositiveIntegerField(default=1)
     startDateTime = models.DateTimeField()
     expiryDateTime = models.DateTimeField()
-    maxApplyCountPerUser = models.IntegerField(default=1)
-    minValue = models.IntegerField()
+    maxApplyCountPerUser = models.PositiveIntegerField(default=1)
+    minValue = models.PositiveIntegerField(default = 1)
     status = models.BooleanField(default = True)
 
     def __str__(self):
         return self.couponCode
+
+    def clean(self):
+        if self.startDateTime > self.expiryDateTime:
+            raise ValidationError("startDateTime must be less than expiryDateTime")
+        super(Coupon, self).clean()
